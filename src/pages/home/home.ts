273546e4600bdc3c435d136edfd5babbebc2dd66
Alex <t.tasselli@gmail.com>
2017-09-26 09:17:21 +0200

    prototipo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,117 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public countClick = 0;
+  public bState = false;
 
-  constructor(public navCtrl: NavController) {
+  
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
   }
+  
+  
+  getPosition() {
+    this.geolocation.getCurrentPosition().then((position) => {
+      alert(position.coords.latitude + " " + position.coords.longitude);
+    });
+  } 
 
+	
+  Botton_Press_Release(idBotton,idEvt)
+   {
+   switch(idBotton)
+      {
+	  case 'bN':
+	        switch(idEvt)
+			{
+			case 'D':
+			   console.log("Motor Nord = ON");
+			   this.bState = true;
+			   break; 
+			case 'U':
+			   console.log("Motor Nord = OFF");
+			   this.bState = false;
+			   break;
+			case 'O':
+			   if(this.bState==true)
+			     {
+			     console.log("Motor Nord = OFF");
+				 this.bState = true;
+				 }
+			   break;
+			}
+			break;
+
+	  case 'bS':
+	        switch(idEvt)
+			{
+			case 'D':
+			   console.log("Motor Sud = ON");
+			   this.bState = true;
+			   break; 
+			case 'U':
+			   console.log("Motor Sud = OFF");			
+			   this.bState = false;
+			   break;
+			case 'O':
+			   if(this.bState==true)
+			     {
+			     console.log("Motor Sud = OFF");
+				 this.bState = true;
+				 }
+			   break;
+			}
+			break;
+
+	  case 'bW':
+	        switch(idEvt)
+			{
+			case 'D':
+			   console.log("Motor West = ON");
+			   this.bState = true;
+			   break; 
+			case 'U':
+			   console.log("Motor West = OFF");			
+			   this.bState = false;
+			   break;
+			case 'O':
+			   if(this.bState==true)
+			     {
+			     console.log("Motor West = OFF");
+				 this.bState = true;
+				 }
+			   break;   
+			}
+			break;
+
+	  case 'bE':
+	        switch(idEvt)
+			{
+			case 'D':
+			   console.log("Motor Est = ON");
+			   this.bState = true;
+			   break; 
+			case 'U':
+			   console.log("Motor Est = OFF");			
+			   this.bState = false;
+			   break;
+			case 'O':
+			   if(this.bState==true)
+			     {
+			     console.log("Motor Est = OFF");
+				 this.bState == true;
+				 }
+			   break;
+			}
+			break;
+
+	  }
+   }   
+	
 }
